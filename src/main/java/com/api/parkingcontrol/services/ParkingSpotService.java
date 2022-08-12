7d34a720ebc3ea2e27f618dd00d7be1c9bc286b8
Leonardo Aragão <lewoaragao@gmail.com>
2022-08-12 13:32:14 -0300

    criação de novos pacotes
    controller, model, repository e service

---
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,15 @@
+package com.api.parkingcontrol.services;
+
+import org.springframework.stereotype.Service;
+
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+
+@Service
+public class ParkingSpotService {
+	
+	ParkingSpotRepository parkingSpotRepository;
+
+	public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+		this.parkingSpotRepository = parkingSpotRepository;
+	}
+}
