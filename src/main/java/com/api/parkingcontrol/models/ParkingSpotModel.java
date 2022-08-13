092f020feab66e0f6f46b0084165ad41ab89396b
Leonardo Aragão <lewoaragao@gmail.com>
2022-08-12 23:12:41 -0300

    Criação de métodos Post e Get

--- src/main/java/com/api/parkingcontrol/models/ParkingSpotModel.java
+++ src/main/java/com/api/parkingcontrol/models/ParkingSpotModel.java
@@ -15,41 +15,38 @@ import javax.persistence.Table;
 @Table(name = "TB_PARKING_SPOT")
 public class ParkingSpotModel implements Serializable{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private UUID id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
 	
-	@Column(nullable = false, unique = true, length = 10)
-	private String parkingSpotNumber;
-
-	@Column(nullable = false, unique = true, length = 7)
-	private String licensePlateCar;
-	
-	@Column(nullable = false, length = 70)
-	private String brandCar;
-	
-	@Column(nullable = false, length = 70)
-	private String modelCarl;
-	
-	@Column(nullable = false, length = 70)
-	private String colorCar;
-	
-	@Column(nullable = false)
-	private LocalDateTime registrationDate;
-	
-	@Column(nullable = false, length = 130)
-	private String responsibleName;
-	
-	@Column(nullable = false, length = 30)
-	private String apartment;
-	
-	@Column(nullable = false, length = 30)
-	private String block;
+    @Column(nullable = false, unique = true, length = 10)
+    private String parkingSpotNumber;
+    
+    @Column(nullable = false, unique = true, length = 7)
+    private String licensePlateCar;
+    
+    @Column(nullable = false, length = 70)
+    private String brandCar;
+    
+    @Column(nullable = false, length = 70)
+    private String modelCar;
+    
+    @Column(nullable = false, length = 70)
+    private String colorCar;
+    
+    @Column(nullable = false)
+    private LocalDateTime registrationDate;
+    
+    @Column(nullable = false, length = 130)
+    private String responsibleName;
+    
+    @Column(nullable = false, length = 30)
+    private String apartment;
+    
+    @Column(nullable = false, length = 30)
+    private String block;
 
 	public UUID getId() {
 		return id;
@@ -83,12 +80,12 @@ public class ParkingSpotModel implements Serializable{
 		this.brandCar = brandCar;
 	}
 
-	public String getModelCarl() {
-		return modelCarl;
+	public String getModelCar() {
+		return modelCar;
 	}
 
-	public void setModelCarl(String modelCarl) {
-		this.modelCarl = modelCarl;
+	public void setModelCar(String modelCar) {
+		this.modelCar = modelCar;
 	}
 
 	public String getColorCar() {
@@ -130,4 +127,5 @@ public class ParkingSpotModel implements Serializable{
 	public void setBlock(String block) {
 		this.block = block;
 	}
+    
 }
